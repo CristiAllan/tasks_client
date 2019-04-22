2be1061bd7a5b7c91f7f120c35054df5d203f7af
CristiAllan <cristiallan@gmail.com>
2019-04-22 09:41:50 -0300

    add timer example to the page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.scss';
 import Container from 'react-bootstrap/Container'
 import Header from './components/header/Header';
 import Tasks from './components/tasks/Tasks';
+import Timer from './components/timer/Timer';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheckCircle, faTrashAlt} from '@fortawesome/free-solid-svg-icons'
@@ -15,6 +16,7 @@ class App extends Component {
     return (
       <div>
         <Header />
+        <Timer />
         <Container>
           <Tasks />
         </Container>
